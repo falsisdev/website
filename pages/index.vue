d8496c8086fe9717eb7b0844f551d030a4a317a3
Falsis <63756985+falsisdev@users.noreply.github.com>
2022-06-28 13:24:42 +0300

    fix: fixed activity card

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
   <div v-else-if="lanyardActivities.activity.check == true" class="mt-5">
    <div class="flex pb-[25px] pt-[25px] bg-[#080808] break-word rounded-b-[10px]">
        <!--<img v-if="lanyardActivities.activity.name = 'Brawlhalla'" src="https://www.brawlhalla.com/c/uploads/2021/07/hattori.png" class="ml-8 rounded-md w-[8rem] h-[8rem]">-->
-      <img v-else-if="lanyardActivities.activity['image'].charAt(0) + lanyardActivities.activity['image'].charAt(1) + lanyardActivities.activity['image'].charAt(2) == 'mp:' == true" :src="`https://${lanyardActivities.activity['image'].slice(62)}`" class="ml-8 rounded-md w-[8rem] h-[8rem]">
+      <img v-if="lanyardActivities.activity['image'].charAt(0) + lanyardActivities.activity['image'].charAt(1) + lanyardActivities.activity['image'].charAt(2) == 'mp:' == true" :src="`https://${lanyardActivities.activity['image'].slice(62)}`" class="ml-8 rounded-md w-[8rem] h-[8rem]">
       <img v-else :src="`https://cdn.discordapp.com/app-assets/${lanyardActivities.activity.appid}/${lanyardActivities.activity['image']}.png`" class="ml-8 rounded-md w-[8rem] h-[8rem]">
       <div class="ml-4">
         <span class="absolute pl-[10px] text-red-500 text-1x2">{{ lanyardActivities.activity.name || "" }}</span>
