02be20d58139a951659e531d10f1b009d2c46d15
Falsis <keremtokul@hotmail.com>
2026-07-23 17:05:28 +0300

    chore: add error handler for vercel deployment

--- api/index.go
+++ api/index.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/falsisdev/website/web"
 )
 
-var tmpl *template.Template
+var (
+	tmpl    *template.Template
+	initErr error
+)
 
 var funcMap = template.FuncMap{
 	"age": func(milliseconds int64) string {
@@ -20,19 +24,20 @@ var funcMap = template.FuncMap{
 }
 
 func init() {
-	var err error
-	tmpl, err = template.New("").Funcs(funcMap).ParseFS(web.Files,
+	tmpl, initErr = template.New("").Funcs(funcMap).ParseFS(web.Files,
 		"templates/*.html",
 		"templates/layouts/*.html",
 		"templates/components/*.html",
 	)
-	if err != nil {
-		panic(err)
-	}
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if len(r.URL.Path) >= 8 && r.URL.Path[:8] == "/static/" {
+	if initErr != nil {
+		http.Error(w, fmt.Sprintf("Template Init Error: %v", initErr), http.StatusInternalServerError)
+		return
+	}
+
+	if strings.HasPrefix(r.URL.Path, "/static/") {
 		http.FileServer(http.FS(web.Files)).ServeHTTP(w, r)
 		return
 	}
@@ -43,6 +48,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	err := tmpl.ExecuteTemplate(w, "index.html", data)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Render Error: %v", err), http.StatusInternalServerError)
 	}
 }
